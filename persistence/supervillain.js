be73c7c0b6bc5c66b456e87e9f1fe5269c815049
Shaf07 <aaronkarim21@gmail.com>
2022-05-23 13:01:33 +0100

    final before lunch

--- persistence/supervillain.js
+++ persistence/supervillain.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
 const supervillainSchema = new Schema({
-    name: String,
-    superpower: String
-})
+    name: {
+        type: String,
+        required: true
+    },
+    superpower: {
+        type: String,
+        required: true
+    },
+    age: Number
+});
+
+const supervillain = model('supervillain', supervillainSchema);
+
+module.export = supervillain;
